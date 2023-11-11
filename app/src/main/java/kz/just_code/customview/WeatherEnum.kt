4601532a3b30caa5998b1cdb7fae0db7e9a5a916
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-11-11 15:57:19 +0600

    feature: added elements

---
+++ app/src/main/java/kz/just_code/customview/WeatherEnum.kt
@@ -0,0 +1,9 @@
+package kz.just_code.customview
+
+enum class TimeOfDay {
+    DAY, NIGHT
+}
+
+enum class PrecipitationType {
+    LIGHT_RAIN, HEAVY_RAIN, LIGHT_SNOW, HEAVY_SNOW
+}
--- app/src/main/java/kz/just_code/customview/view/WeatherView.kt
+++ app/src/main/java/kz/just_code/customview/view/WeatherView.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
+import kz.just_code.customview.PrecipitationType
 import kz.just_code.customview.R
 import kz.just_code.customview.TimeOfDay
 import kz.just_code.customview.databinding.ViewWeatherBinding
@@ -17,21 +18,57 @@ class WeatherView @JvmOverloads constructor(
     private val binding: ViewWeatherBinding =
         ViewWeatherBinding.inflate(LayoutInflater.from(context), this)
 
-    var pauseClick: (() -> Unit)? = null
-    var likeClick: (() -> Unit)? = null
+//    var pauseClick: (() -> Unit)? = null
+//    var likeClick: (() -> Unit)? = null
 
     init {
         setAttrs(attrs, R.styleable.WeatherView) {
             binding.temp.text = it.getString(R.styleable.WeatherView_weather_temp)
-            binding.cloud.setImageResource(it.getResourceId(R.styleable.WeatherView_weather_cloud, R.drawable.ic_rain))
-            val timeOfDay = TimeOfDay.values().firstOrNull { time ->
-                time.ordinal == it.getInt(R.styleable.WeatherView_weather_times_of_day, 0)
-            }
-            binding.time.text = timeOfDay?.name
+            binding.city.text = it.getString(R.styleable.WeatherView_weather_city)
+            binding.country.text = it.getString(R.styleable.WeatherView_weather_country)
+            // Extracting the drawable resource ID based on the weather_times_of_day attribute
+            val timeOfDayDrawableId =
+                when (it.getInt(R.styleable.WeatherView_weather_times_of_day, 0)) {
+                    TimeOfDay.DAY.ordinal -> R.drawable.ic_sun
+                    TimeOfDay.NIGHT.ordinal -> R.drawable.ic_moon
+                    else -> R.drawable.ic_sun // Default for unknown time of day
+                }
+            binding.time.setImageResource(timeOfDayDrawableId)
 
-            binding.imageView.setOnClickListener {
-                pauseClick?.invoke()
+            val precipitationTypeDrawableId =
+                when (it.getInt(R.styleable.WeatherView_weather_precipitation_type, 0)) {
+                    PrecipitationType.LIGHT_RAIN.ordinal -> R.drawable.ic_light_rain
+                    PrecipitationType.HEAVY_RAIN.ordinal -> R.drawable.ic_heavy_rain
+                    PrecipitationType.LIGHT_SNOW.ordinal -> R.drawable.ic_light_snow
+                    PrecipitationType.HEAVY_SNOW.ordinal -> R.drawable.ic_heavy_snow
+                    else -> R.drawable.ic_light_rain
+                }
+            binding.precipitationType.setImageResource(precipitationTypeDrawableId)
+
+            val hasWind = it.getBoolean(R.styleable.WeatherView_weather_has_wind, false)
+            if (hasWind){
+                binding.windyOrNot.text = "No wind"
+            } else {
+                binding.windyOrNot.text = "Wind"
             }
+//            val timeOfDay = TimeOfDay.values().firstOrNull { time ->
+//                time.ordinal == it.getInt(R.styleable.WeatherView_weather_times_of_day, 0)
+//            }
+//            val timeOfDayDrawableId = it.getResourceId(
+//                R.styleable.WeatherView_weather_times_of_day,
+//                R.drawable.ic_sun // Default drawable for daytime
+//            )
+//            val timeOfDayDrawableId = it.getResourceId(
+//                R.styleable.WeatherView_weather_times_of_day,
+//                R.drawable.ic_sun // Default drawable for daytime
+//            )
+//
+//            binding.time.setImageResource(timeOfDayDrawableId)
+//
+////
+//            binding.imageView.setOnClickListener {
+//                pauseClick?.invoke()
+//            }
         }
     }
 }
